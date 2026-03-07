8b6d6bdfc72341c5838674cdc23404d13306186e
marykuo <meiyu19980928@gmail.com>
2026-03-07 21:38:18 +0800

    feat(hash-legacy-nested-lazy): implement lazy loading and show PageLoader when loading

--- packages/hash-legacy-nested-lazy/src/App.jsx
+++ packages/hash-legacy-nested-lazy/src/App.jsx
@@ -1,7 +1,11 @@
+import { Suspense, lazy } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import PageLoader from "./components/PageLoader";
+
+// use lazy loading
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
 
 function App() {
   return (
@@ -9,13 +13,16 @@ function App() {
       {/* Navbar component, shared across all pages */}
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+      {/* use Suspense to wrap and provide a loading fallback */}
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        {/* catch-all route for undefined paths */}
-        <Route path="*" element={<Home />} />
-      </Routes>
+          {/* catch-all route for undefined paths */}
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
 }
