8829247ab7f9f192af11d57568b0a03554e5799e
marykuo <meiyu19980928@gmail.com>
2026-03-07 21:01:26 +0800

    feat(hash-data-nested-lazy): show PageLoader when loading

--- packages/hash-data-nested-lazy/src/pages/MainLayout.jsx
+++ packages/hash-data-nested-lazy/src/pages/MainLayout.jsx
@@ -1,13 +1,24 @@
-import { Outlet, Link } from "react-router-dom";
+import { useNavigation, Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import PageLoader from "../components/PageLoader";
 
 function MainLayout() {
+  const navigation = useNavigation();
+
+  // lazy load 時，state 會變成 "loading"
+  const isLoading = navigation.state === "loading";
+
   return (
     <>
       <Navbar />
       <main>
-        {/* 子路由元件將會渲染在這裡 */}
-        <Outlet />
+        {isLoading ? (
+          // 顯示暫時的載入畫面，直到 lazy 元件載入完成
+          <PageLoader />
+        ) : (
+          // child route component 會在 lazy 元件載入完成後顯示
+          <Outlet />
+        )}
       </main>
     </>
   );
