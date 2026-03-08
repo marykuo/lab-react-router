fe83064016b807053c4074d98f83ace04d1989b7
marykuo <meiyu19980928@gmail.com>
2026-03-08 18:11:05 +0800

    feat(hash-declartive): implement NavLink with active style

--- packages/hash-declartive/src/components/Navbar.jsx
+++ packages/hash-declartive/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
 
 export function Navbar() {
   return (
     <nav>
       <Link to="">首頁</Link>
       {" | "}
-      <Link to="about">關於我們</Link>
+      {/* NavLink makes it easy to show active styles */}
+      <NavLink
+        to="about"
+        className={({ isActive }) => (isActive ? "active" : "")}
+        style={({ isActive }) => ({ color: isActive && "red" })}
+      >
+        關於我們
+      </NavLink>
 
       <p>
         {"Layout Routes: "}
