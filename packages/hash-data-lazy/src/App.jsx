da6bbf2254ca0dc9504625261bda35dd4681c777
marykuo <meiyu19980928@gmail.com>
2026-03-08 15:47:42 +0800

    feat: remove duplicate package

--- packages/hash-data-lazy/src/App.jsx
+++ packages/hash-data-lazy/src/App.jsx
@@ -1,6 +1,31 @@
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./pages/MainLayout";
 
+// This is the non global layout version of the router with lazy loading.
+// const router = createHashRouter([
+//   {
+//     path: "/",
+//     lazy: async () => {
+//       const { default: Home } = await import("./pages/Home");
+//       return { Component: Home };
+//     },
+//   },
+//   {
+//     path: "/about",
+//     lazy: async () => {
+//       const { default: About } = await import("./pages/About");
+//       return { Component: About };
+//     },
+//   },
+//   {
+//     path: "*",
+//     lazy: async () => {
+//       const { default: Home } = await import("./pages/Home");
+//       return { Component: Home };
+//     },
+//   },
+// ]);
+
 const router = createHashRouter([
   {
     path: "/",
--- packages/hash-data/src/App.jsx
+++ packages/hash-data/src/App.jsx
@@ -3,6 +3,22 @@ import MainLayout from "./pages/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+// This is the non global layout version of the router.
+// const router = createHashRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/about",
+//     element: <About />,
+//   },
+//   {
+//     path: "*", // Catch-all route
+//     element: <Home />,
+//   },
+// ]);
+
 const router = createHashRouter([
   {
     path: "/",
