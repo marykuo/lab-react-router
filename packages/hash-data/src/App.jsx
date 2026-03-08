4b59691765620df8b5e25b6432501b4c4c8bd83d
marykuo <meiyu19980928@gmail.com>
2026-03-08 21:27:32 +0800

    feat(hash-data): implement hash router
    - Setup HashRouter with Home and About routes
    - Setup  wildcard path redirecting to NotFound page

--- packages/hash-data/src/App.jsx
+++ packages/hash-data/src/App.jsx
@@ -1,9 +1,25 @@
+import { createHashRouter, RouterProvider } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const router = createHashRouter([
+  {
+    index: true,
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
